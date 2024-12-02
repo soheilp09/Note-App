e50fc3871cae53ce80f5287896081b84dbde9f2b
SoheilAsgari <soheilasgari183@gmail.com>
2024-12-03 03:10:02 +0330

    SortNotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-
 import { useState } from 'react'
 import AddNewNote from './AddNewNote'
 import './App.css'
@@ -11,13 +10,16 @@ import NoteStatus from './NoteStatus';
 function App() {
 
   const [notes,setNotes] = useState([]);
+  const [sort,setSort] = useState("latest");
 
 
+  //HandelDelete
 function handeldelete(id){
 const filterdNotes =   notes.filter((item)=>item.id != id);
 setNotes(filterdNotes)
 };
 
+//ToggleComponent
 function handelToggle(id) {
 
  const newNote =  notes.map((item)=>item.id === id ?{...item,completed: !item.completed} : item)
@@ -25,17 +27,26 @@ function handelToggle(id) {
 
 };
 
+//SortNotes
+let sortedNotes = notes;
+if(sort === "earliest")sortedNotes.sort((a,b)=>new Date(a.creaditAt) - new Date(b.creaditAt))
+  if(sort === "latest")sortedNotes.sort((a,b)=>new Date(b.creaditAt) - new Date(a.creaditAt))
+    if(sort === "completed")sortedNotes.sort((a,b)=>new Date(a.completed) - new Date(b.completed))
+
+
 
   return (
     <div className='container'>
-      <NoteHeader notes={notes.length}/>
+      <NoteHeader setSort={setSort} sort={sort} notes={notes.length}/>
+
       <div className='note-app'>
         <AddNewNote setNotes={setNotes}/>
-        
+
        <div className='note-container'>
        <NoteStatus notes={notes}/>
-       <NoteList notes={notes} onDelete={handeldelete} onToggle={handelToggle}/>
+       <NoteList notes={sortedNotes} onDelete={handeldelete} onToggle={handelToggle}/>
        </div>
+
       </div>
 
     </div>
--- src/NoteHeader.jsx
+++ src/NoteHeader.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function NoteHeader({notes}) {
+function NoteHeader({notes,sort,setSort}) {
   return (
 <div className='note-header'>
     <h2>My Notes({notes})</h2>
+
+ <select onChange={(e)=>setSort(e.target.value)} value={sort}>
+  <option   value="latest">Latest</option>
+  <option   value="earliest">Earliest</option>
+  <option   value="completed">Completed</option>
+ </select>
 
 
 </div>
