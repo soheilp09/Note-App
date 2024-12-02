854c380c2bd3414eb9908167a3a0c6e354ecf9aa
SoheilAsgari <soheilasgari183@gmail.com>
2024-12-02 23:57:42 +0330

    AppUI

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <div className='note-header'>noteHeader</div>
+      <div className='note-app'>
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
